436ac4adc044360ff29a3ac6311e06bf5e303fd4
wtfcadu <80596766+wtfcadu@users.noreply.github.com>
2025-05-15 01:20:09 +0300

    Add files via upload

--- src/components/Scene/Model.js
+++ src/components/Scene/Model.js
@@ -13,18 +13,23 @@ export default function Model() {
     })
 
     const materialProps = useControls({
-        thickness: { value: 0.2, min: 0, max: 3, step: 0.05 },
-        roughness: { value: 0, min: 0, max: 1, step: 0.1 },
-        transmission: {value: 1, min: 0, max: 1, step: 0.1},
-        ior: { value: 1.2, min: 0, max: 3, step: 0.1 },
-        chromaticAberration: { value: 0.02, min: 0, max: 1},
-        backside: { value: true},
+        thickness: { value: 3.0, min: 0, max: 3, step: 0.05 },
+        roughness: { value: 0.1, min: 0, max: 1, step: 0.1 },
+        transmission: {value: 1.0, min: 0, max: 1, step: 0.1},
+        ior: { value: 3.0, min: 0, max: 3, step: 0.1 },
+        chromaticAberration: { value: 0.49, min: 0, max: 1},
+        backside: { value: false},
+        distortion: { value: 1.0, min: 0, max: 1, step: 0.1 },
+        blur: { value: 1.0, min: 0, max: 1, step: 0.1 },
+        resolution: { value: 2048, min: 256, max: 2048, step: 64 },
+        samples: { value: 16, min: 1, max: 32, step: 1 },
+        temporalDistortion: { value: 0.1, min: 0, max: 1, step: 0.1 },
     })
     
     return (
         <group scale={viewport.width / 200} >
-            <Text font={'/fonts/PPNeueMontreal-Bold.otf'} position={[0, 0, -1]} fontSize={20} color="white" anchorX="center" anchorY="middle">
-                hello world!
+            <Text font={'/fonts/NotoSerifDisplay-ExtraCondensedMedium.ttf'} position={[0, 0, -1]} fontSize={20} color="white" anchorX="center" anchorY="middle" textAlign="center">
+                {"EMPOWER\nGLOBAL\nINNOVATION"}
             </Text>
             <mesh ref={torus} {...nodes.Cone001}>
                 <MeshTransmissionMaterial {...materialProps}/>
