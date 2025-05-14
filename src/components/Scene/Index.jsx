940cff95ec49831e25ba4a5f539dfa4438562b43
wtfcadu <80596766+wtfcadu@users.noreply.github.com>
2025-05-15 01:22:51 +0300

    Add files via upload

--- src/components/Scene/Index.jsx
+++ src/components/Scene/Index.jsx
@@ -2,28 +2,13 @@
 import { Canvas } from '@react-three/fiber'
 import Model from './Model';
 import { Environment } from '@react-three/drei'
-import { LevaProvider } from 'leva'
 
 export default function Index() {
   return (
-    <LevaProvider
-      theme={{
-        colors: {
-          elevation1: 'rgba(0, 0, 0, 0.8)',
-        },
-        styles: {
-          panel: {
-            backdropFilter: 'blur(10px)',
-            WebkitBackdropFilter: 'blur(10px)',
-          }
-        }
-      }}
-    >
-      <Canvas style={{background: 'black'}}>
-          <Model />
-          <directionalLight intensity={0.5} position={[10, 2, 8]}/>
-          <Environment preset="night" />
-      </Canvas>
-    </LevaProvider>
+    <Canvas style={{background: '#000000'}}>
+        <Model />
+        <directionalLight intensity={2} position={[0, 2, 3]}/>
+        <Environment preset="city" />
+    </Canvas>
   )
 }
